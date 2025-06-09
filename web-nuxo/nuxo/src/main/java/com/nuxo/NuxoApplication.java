c252faa573b4651edf5c5928096adb5699f871b1
Bruno <obruno26@outlook.com>
2025-06-09 16:04:04 -0300

    feature/create scaffolding

---
+++ web-nuxo/nuxo/src/main/java/com/nuxo/NuxoApplication.java
@@ -0,0 +1,13 @@
+package com.nuxo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuxoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuxoApplication.class, args);
+	}
+
+}
---
+++ web-nuxo/nuxo/src/test/java/com/nuxo/NuxoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuxo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuxoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
